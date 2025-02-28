61a0acac3614e4f8ddd69b70b9cd51f028820488
m00nwtchr <m00nwtchr@duck.com>
2025-02-28 12:14:03 +0100

    gpg

--- hosts/m00nsrv/default.nix
+++ hosts/m00nsrv/default.nix
@@ -84,7 +84,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    gpg
+    gnupg
   ];
 
   security.tpm2.enable = true;
