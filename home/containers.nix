ec8a0ca7ad8e024d87d6b3574f8834a92af519eb
m00nwtchr <m00nwtchr@duck.com>
2025-06-08 14:09:10 +0200

    k3d/podman

---
+++ home/containers.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  system,
+  osConfig,
+  ...
+}: {
+  home.packages = with pkgs; [
+    # Kubernetes
+    kubectl
+    kubelogin-oidc
+    kubernetes-helm
+    cilium-cli
+    k3d
+
+    docker-compose
+
+    lens
+  ];
+
+  services.podman = {
+    enable = true;
+    autoUpdate.enable = true;
+    settings.storage.storage.driver = lib.mkForce osConfig.virtualisation.containers.storage.settings.storage.driver;
+  };
+
+  systemd.user.sockets.podman = {
+    Unit = {
+      Description = "Podman API Socket";
+      Documentation = "man:podman-system-service(1)";
+    };
+    Socket = {
+      ListenStream = "%t/podman/podman.sock";
+      SocketMode = 0660;
+    };
+    Install = {
+      WantedBy = ["sockets.target"];
+    };
+  };
+}
