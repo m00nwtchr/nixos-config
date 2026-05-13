14b6e3d731707cb6111fe469aa4ddebbd91febc7
m00nwtchr <m00n@naktis.eu>
2026-05-14 00:26:27 +0200

    nvidia modules

--- systems/x86_64-linux/ganymede/default.nix
+++ systems/x86_64-linux/ganymede/default.nix
@@ -20,19 +20,15 @@
 
   hardware.nvidia = {
     open = false;
-    # package = config.boot.kernelPackages.nvidiaPackages.production;
     package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
   };
-  # nixpkgs.config.allowUnfree = true;
-  nixpkgs.config = {
-    allowUnfreePredicate = pkg:
-      builtins.elem (lib.getName pkg) [
-        "nvidia-x11"
-      ];
-  };
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "nvidia-x11"
+      "nvidia-kernel-modules"
+    ];
 
   networking.hostId = lib.mkForce "8504e2ee";
-
   networking.hosts = {
     # "2a02:a313:43e4:7080::200" = ["m00nlit.dev" "external.m00nlit.dev" "idm.m00nlit.dev"];
     # "2a02:a313:43e4:7080::7dc5" = ["idm.m00nlit.dev" "m00nlit.dev" "m00nsrv"];
@@ -56,9 +52,9 @@
   services.nfs.server.enable = true;
   services.seatd.enable = true;
   services.openiscsi = {
-    enable=true;
+    enable = true;
     name = "iqn.2016-04.com.open-iscsi:bd68ae22efed";
-};
+  };
   systemd.network.links."10-lan" = {
     matchConfig = {
       MACAddress = "9c:6b:00:08:bb:03";
@@ -226,32 +222,31 @@
     stPkgs = import inputs.stable {inherit system;};
 
     crioPackage = stPkgs.cri-o.override {
-    extraPackages =
-      config.virtualisation.cri-o.extraPackages
-      ++ lib.optional (config.boot.supportedFilesystems.zfs or false) config.boot.zfs.package;
-  };
-
-     in {
+      extraPackages =
+        config.virtualisation.cri-o.extraPackages
+        ++ lib.optional (config.boot.supportedFilesystems.zfs or false) config.boot.zfs.package;
+    };
+  in {
     package = crioPackage;
     settings = {
-    crio.runtime = {
-      # log_level=lib.mkForce "debug";
-      # default_runtime = "nvidia";
-      runtimes.nvidia = {
-        runtime_path = "${pkgs.nvidia-container-toolkit.tools}/bin/nvidia-container-runtime";
-        # runtime_path = "${pkgs.nvidia-container-toolkit}/bin/nvidia-container-runtime";
-        runtime_type = "oci";
+      crio.runtime = {
+        # log_level=lib.mkForce "debug";
+        # default_runtime = "nvidia";
+        runtimes.nvidia = {
+          runtime_path = "${pkgs.nvidia-container-toolkit.tools}/bin/nvidia-container-runtime";
+          # runtime_path = "${pkgs.nvidia-container-toolkit}/bin/nvidia-container-runtime";
+          runtime_type = "oci";
+        };
+        runtimes.kata = {
+          runtime_path = "${pkgs.kata-runtime}/bin/containerd-shim-kata-v2";
+          runtime_type = "vm";
+          runtime_root = "/run/vc";
+          privileged_without_host_devices = true;
+        };
       };
-      runtimes.kata = {
-        runtime_path = "${pkgs.kata-runtime}/bin/containerd-shim-kata-v2";
-        runtime_type = "vm";
-        runtime_root = "/run/vc";
-        privileged_without_host_devices = true;
-      };
-
+      crio.image.image_volumes = "mkdir";
     };
-    crio.image.image_volumes = "mkdir";
-  };};
+  };
 
   environment.etc."nvidia-container-runtime/config.toml".text = ''
     [nvidia-container-runtime]
