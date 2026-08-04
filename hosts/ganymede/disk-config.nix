6f6e1e70a64952f2bb6c95021933b68057d672f2
m00nwtchr <m00n@naktis.eu>
2026-08-04 04:32:38 +0200

    feat(ganymede): port disko zfs-on-luks disk-config

---
+++ hosts/ganymede/disk-config.nix
@@ -0,0 +1,251 @@
+{lib, ...}: {
+  environment.etc.crypttab = {
+    mode = "0600";
+    text = ''
+      # <volume-name> <encrypted-device> [key-file] [options]
+      vault-0 UUID=45507ea6-cda1-4ca3-9c49-008b84b0f10f /root/keys/vault.key
+    '';
+  };
+
+  boot.zfs.extraPools = ["rpool" "spark" "vault"];
+
+  disko.zfs.enable = true;
+  disko.devices = {
+    disk = {
+      root = {
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-Micron_7450_MTFDKBA960TFR_24334AA93946";
+
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/efi";
+                mountOptions = [
+                  "fmask=0022"
+                  "dmask=0022"
+                  "umask=0077"
+                ];
+              };
+            };
+
+            root = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "root";
+                settings = {
+                  allowDiscards = true;
+                  bypassWorkqueues = true;
+                };
+                content = {
+                  type = "zfs";
+                  pool = "rpool";
+                };
+              };
+            };
+          };
+        };
+      };
+
+      spark-0 = {
+        type = "disk";
+        device = "/dev/disk/by-id/wwn-0x5002538e0996c7bc";
+
+        content = {
+          type = "zfs";
+          pool = "spark";
+        };
+      };
+      spark-1 = {
+        type = "disk";
+        device = "/dev/disk/by-id/wwn-0x5002538e0996c831";
+
+        content = {
+          type = "zfs";
+          pool = "spark";
+        };
+      };
+
+      vault-0 = {
+        type = "disk";
+        device = "/dev/disk/by-id/wwn-0x5000c500dbb1344b";
+
+        content = {
+          type = "luks";
+          name = "vault-0";
+          initrdUnlock = false;
+          content = {
+            type = "zfs";
+            pool = "vault";
+          };
+        };
+      };
+    };
+
+    zpool = {
+      rpool = {
+        type = "zpool";
+
+        options = {
+          ashift = 12;
+          autotrim = "on";
+          acltype = "posixacl";
+          xattr = "sa";
+          dnodesize = "auto";
+          relatime = "on";
+        };
+        rootFsOptions = {
+          compression = "zstd";
+        };
+
+        datasets = {
+          root = {
+            type = "zfs_fs";
+            mountpoint = "/";
+            options.mountpoint = "legacy";
+          };
+          home = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "/home";
+              atime = "off";
+              devices = "off";
+              setuid = "off";
+            };
+          };
+          nix = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+            options = {
+              mountpoint = "legacy";
+              compression = "zstd-6";
+              atime = "off";
+            };
+          };
+          var = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "none";
+              canmount = "off";
+            };
+          };
+          "var/log" = {
+            type = "zfs_fs";
+            mountpoint = "/var/log";
+            options = {
+              mountpoint = "legacy";
+              compression = "zstd-3";
+              atime = "off";
+              recordsize = "16K";
+            };
+          };
+          "var/lib" = {
+            type = "zfs_fs";
+            mountpoint = "/var/lib";
+            options.mountpoint = "legacy";
+          };
+          "var/lib/kubelet" = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "/var/lib/kubelet";
+              atime = "off";
+              recordsize = "16K";
+            };
+          };
+          "var/lib/containers" = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "/var/lib/containers";
+              atime = "off";
+              recordsize = "16K";
+            };
+          };
+        };
+      };
+
+      spark = {
+        type = "zpool";
+        mode = "mirror";
+
+        options = {
+          mountpoint = "/spark";
+          ashift = 12;
+          autotrim = "on";
+          acltype = "posixacl";
+          xattr = "sa";
+          dnodesize = "auto";
+        };
+
+        datasets = {
+          data = {
+            type = "zfs_fs";
+            options = {
+              canmount = "off";
+              mountpoint = "none";
+
+              atime = "off";
+              compression = "zstd";
+              relatime = "off";
+            };
+          };
+
+          # "data/git" = {
+          #   type = "zfs_fs";
+          #   options = {
+          #     recordsize = "16K";
+          #   };
+          # };
+        };
+      };
+
+      vault = {
+        type = "zpool";
+        # mode = "mirror";
+
+        options = {
+          mountpoint = "/vault";
+          ashift = 12;
+          autotrim = "off";
+          acltype = "posixacl";
+          xattr = "sa";
+        };
+
+        datasets = {
+          data = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "none";
+              canmount = "off";
+              atime = "off";
+              compression = "zstd";
+            };
+          };
+
+          "data/media" = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "legacy";
+              recordsize = "1M";
+              primarycache = "metadata";
+            };
+          };
+
+          media = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "legacy";
+              recordsize = "1M";
+              primarycache = "metadata";
+            };
+          };
+        };
+      };
+    };
+  };
+}
